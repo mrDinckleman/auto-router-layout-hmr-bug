33d8a3f3d0284a074faf6fffa605940853ece74e
Sergei Kovalchuk <sergei.v.kovalchuk@gmail.com>
2024-11-26 16:07:26 +0100

    Add unplugin-vue-router to vue-meta-layouts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import MetaLayouts from 'vite-plugin-vue-meta-layouts';
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter(),
     vue(),
     MetaLayouts({
       defaultLayout: 'LayoutDefault',
