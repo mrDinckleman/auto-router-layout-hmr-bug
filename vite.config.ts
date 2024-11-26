3d722b63aff1d9e372a3183741ab495860b2245f
Sergei Kovalchuk <sergei.v.kovalchuk@gmail.com>
2024-11-26 16:07:26 +0100

    Add unplugin-vue-router to vue-layouts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Layouts from 'vite-plugin-vue-layouts';
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter(),
     vue(),
     Layouts({
       defaultLayout: 'LayoutDefault',
