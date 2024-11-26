a19c987130f4b1b8a8796cd5dd8b19bada74424e
Sergei Kovalchuk <sergei.v.kovalchuk@gmail.com>
2024-11-26 16:21:54 +0100

    Install vite-plugin-vue-layouts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Layouts from 'vite-plugin-vue-layouts';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Layouts({
+      defaultLayout: 'LayoutDefault',
+    }),
     vueDevTools(),
   ],
   resolve: {
