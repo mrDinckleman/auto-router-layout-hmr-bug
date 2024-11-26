42188b3639974bc7db93af7d12c78a44af925a12
Sergei Kovalchuk <sergei.v.kovalchuk@gmail.com>
2024-11-26 16:21:54 +0100

    Install vite-plugin-vue-meta-layouts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import MetaLayouts from 'vite-plugin-vue-meta-layouts';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    MetaLayouts({
+      defaultLayout: 'LayoutDefault',
+    }),
     vueDevTools(),
   ],
   resolve: {
